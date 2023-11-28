d8ff27e3762bd49729113f2faf5c8f505bee54b3
Louis <lstephens@chumneyads.com>
2023-11-28 14:51:24 -0500

    feat: changes time to min and seconds

--- src/components/PouroverCalculator.tsx
+++ src/components/PouroverCalculator.tsx
@@ -15,11 +15,18 @@ const CalculatePourover = ({}: CalculatePouroverProps) => {
 	const calculateTotalWater = (coffeeWeight: number, coffeeWaterRatio: number) => coffeeWeight * coffeeWaterRatio;
 
 	const calculatePours = (currentBrewMethod: number, coffeeWaterRatio: number, coffeeWeight: number, totalWater: number) => {
-		return brewMethods[currentBrewMethod].steps.map((steps, i) => ({
-			description: steps.description,
-			amount: steps.amount(coffeeWaterRatio, coffeeWeight, totalWater),
-			time: i * 45,
-		}));
+		return brewMethods[currentBrewMethod].steps.map((step, i) => {
+			const timeInSeconds = i * 45;
+			const minutes = Math.floor(timeInSeconds / 60);
+			const seconds = timeInSeconds % 60;
+			const formattedTime = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+
+			return {
+				description: step.description,
+				amount: Math.round(step.suggestedRatio * totalWater),
+				time: formattedTime,
+			};
+		});
 	};
 
 	const handleCalculatePours = () => {
@@ -32,27 +39,48 @@ const CalculatePourover = ({}: CalculatePouroverProps) => {
 	return (
 		<>
 			<div className="flex flex-col gap-8">
-				<select className="rounded-lg border-violet-400" value={currentBrewMethod} onChange={(e) => setCurrentBrewMethod(Number(e.target.value))}>
-					{brewMethods.map((method, index) => (
-						<option value={index} key={index}>
-							{method.name}
-						</option>
-					))}
-				</select>
-				<input
-					className="rounded-lg border-violet-400"
-					type="number"
-					value={coffeeWeight}
-					onChange={(e) => setCoffeeWeight(Number(e.target.value))}
-					placeholder="Amount of coffee in grams"
-				/>
-				<input
-					className="rounded-lg border-violet-400"
-					type="number"
-					value={coffeeWaterRatio}
-					onChange={(e) => setCoffeeWaterRatio(Number(e.target.value))}
-					placeholder="Brew ratio"
-				/>
+				<div className="flex flex-col">
+					<label className="mb-2 text-sm font-bold">Pourover Method</label>
+					<select className="rounded-lg border-2 border-violet-400" value={currentBrewMethod} onChange={(e) => setCurrentBrewMethod(Number(e.target.value))}>
+						{brewMethods.map((method, index) => (
+							<option value={index} key={index}>
+								{method.name}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className="flex flex-col">
+					<label className="mb-2 text-sm font-bold">
+						Coffee Weight <span className="text-xs">(grams)</span>
+					</label>
+					<input
+						className="rounded-lg border-2 border-violet-400"
+						type="number"
+						value={coffeeWeight}
+						onChange={(e) => setCoffeeWeight(Number(e.target.value))}
+						placeholder="Amount of coffee in grams"
+					/>
+				</div>
+				<div className="flex flex-col">
+					<label className="mb-2 text-sm font-bold">Ratio</label>
+
+					<div className="flex">
+						<input
+							className="rounded-bl-lg rounded-tl-lg border-b-2 border-l-2 border-r-0 border-t-2 border-violet-400 disabled:bg-slate-200"
+							type="number"
+							value={1}
+							placeholder="Brew ratio"
+							disabled
+						/>
+						<input
+							className="rounded-br-lg rounded-tr-lg border-b-2 border-l-0 border-r-2 border-t-2 border-violet-400"
+							type="number"
+							value={coffeeWaterRatio}
+							onChange={(e) => setCoffeeWaterRatio(Number(e.target.value))}
+							placeholder="Brew ratio"
+						/>
+					</div>
+				</div>
 				<button className="inline-flex self-center rounded-lg bg-black" onClick={handleCalculatePours}>
 					<div className="elative w-full -translate-x-1 -translate-y-1 rounded-lg border-2 border-black bg-violet-400 px-8 py-4 text-lg font-bold transition-all duration-200 ease-in-out hover:translate-x-0 hover:translate-y-0">
 						Calculate
